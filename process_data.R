ad13d449f5f044171f53b45e3da2b8f501ef0515
Matthew Berginski <matthew.berginski@gmail.com>
2018-07-19 09:18:54 -0400

    adding runtime

--- process_data.R
+++ process_data.R
@@ -1,8 +1,13 @@
 library(here)
-# rmarkdown::render(here('bone_marrow/analyze_bone_marrow.Rmd'))
-# 
-# rm(list = ls())
+
+ptm <- proc.time()
+
+rmarkdown::render(here('bone_marrow/analyze_bone_marrow.Rmd'))
+
+rm(list = ls())
 
 rmarkdown::render(here('cord_blood/analyze_cord_blood.Rmd'))
 
 rm(list = ls())
+
+proc.time() - ptm
